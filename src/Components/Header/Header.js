48cdd6a9a7a1c815a2b2f9e19073d45ca335386a
mutlib-ansari <mutlib.indianit@gmail.com>
2024-12-18 18:51:33 +0530

    update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -793,7 +793,7 @@ function Navbar() {
                     </div>
 
                     {/* Main Navbar */}
-                    <div className="flex items-center justify-between py-4 px-6">
+                    <div className=" bg-white md:bg-transparent flex items-center justify-between py-4 px-6">
                         {/* Logo */}
                         <div className="flex items-center space-x-2">
                             <img src={logo} className="h-16 md:h-28" alt="Logo" />
@@ -912,18 +912,18 @@ function Navbar() {
 
                 {/* Hero Section */}
                 <div className="flex flex-wrap p-6">
-                    <div className="w-full md:w-3/5 font-normal mt-auto mb-auto">
+                    <div className="w-full md:w-3/5 font-normal mt-auto mb-auto pt-4 pb-5">
                         <span className="text-lg font-forum text-amber-800">M A J E S T I C R O O M S</span>
-                        <h1 className="font-forum text-[25px] md:text-[60px] leading-[37px] md:leading-[70px] text-gray-800">
+                        <h1 className="font-forum text-[40px]  md:text-[60px] leading-[37px] md:leading-[70px] text-gray-800">
                             Discover Your Paradise <br />
                             Where You Are
                         </h1>
-                        <p className="font-forum text-amber-950 mt-4 pr-6">
+                        <p className=" hidden md:block font-forum text-amber-950 mt-4 pr-6">
                             Sed imperdiet dignissim odio dignissim bibendum. Praesent porttitor
                             tristique velit efficitur, a pulvinar sem hasellus.
                         </p>
 
-                        <Button className="custom-button2 mt-4 mr-5 text-lg font-forum bg-customBrown mb-4">
+                        <Button className="custom-button2 hidden md:block mt-4 mr-5 text-lg font-forum bg-customBrown mb-4">
                             Explore Now
                         </Button>
                     </div>
